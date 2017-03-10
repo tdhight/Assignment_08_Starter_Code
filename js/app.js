c26864598364ef95bdcac39dd1b721d929260c98
tdhight <tonyahight@Tonyas-MacBook-Pro.local>
2017-03-09 19:33:40 -0500

    inserted the value to equal the variable
    html had registered the value as the index in the array and I had to
    assign the variable city to that value

--- js/app.js
+++ js/app.js
@@ -9,16 +9,17 @@ $(document).ready(function(){
 	function newCity() {
 		console.log('this is working');
 		var city = $("#city-type").val();
-		if (city === city[0]) {
+
+		if (city === "0") {
 			$("body").attr("class", "nyc");
-		} else if (city === city[1]) {
+		} else if (city === "1") {
 			console.log('working2')
 			$("body").attr("class", "sf");
-		} else if (city === city[2]) {
-			$("body").attr("class", "LA");
-		} else if (city === city[3]) {
+		} else if (city === "2") {
+			$("body").attr("class", "la");
+		} else if (city === "3") {
 			$("body").attr("class", "austin");
-		} else if (city === city[4]) {
+		} else if (city === "4") {
 			$("body").attr("class", "sydney");	
 		}
 	}
